6a363bc645bc1b8822d210dfd9fe6f706760c558
mhcrnl <mhcrnl@gmail.com>
2017-05-27 17:16:52 +0300

    27/05/2017

--- app.rb
+++ app.rb
@@ -1,4 +1,10 @@
 require_relative "lib/tictactoe"
-
+BEGIN{
+    puts "Comenzile sunt 1 1 x, 0 0 0, etc."
+}
 game = TicTacToe::Game.new
 game.play
+END{
+    puts "Program realizat de practicingruby/tictactoe."
+    puts "GITHUB: https://github.com/mhcrnl/tictactoe/blob/master/app.rb"
+}
